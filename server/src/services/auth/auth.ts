4893b89f3f64a8aa33dc83eff69631a61a54131f
Samuel Silva <samuelsilvawb@gmail.com>
2022-03-14 22:49:43 +0000

    feat(server): create `signOut` mutation

--- server/src/services/auth/auth.ts
+++ server/src/services/auth/auth.ts
@@ -3,6 +3,7 @@ import { Fetch } from "../common/fetch";
 import { isPasswordCorrect } from "./auth-utilities";
 
 type SignInMutationArgs = { email: string; password: string };
+type SignOutMutationArgs = { email: string };
 type User = { id: number; name: string; email: string; hash: string };
 type Session = {
   id: number;
@@ -15,6 +16,7 @@ type Session = {
 export const typeDefs = gql`
   type Mutation {
     signIn(email: String!, password: String!): User
+    signOut(email: String!): User
   }
 `;
 
@@ -50,6 +52,26 @@ export const resolvers = {
         });
 
         return user;
+      }
+
+      return null;
+    },
+    signOut: async (_: unknown, { email }: SignOutMutationArgs) => {
+      const [user]: [User | undefined] =
+        (await Fetch.get(`users?email=${email}`)) || [];
+
+      if (user) {
+        const [userSession]: [Session | undefined] = await Fetch.get(
+          `sessions?userId=${user.id}&signedOut=false`
+        );
+
+        if (userSession) {
+          await Fetch.patch(`sessions/${userSession.id}`, {
+            signedOut: true,
+          });
+
+          return user;
+        }
       }
 
       return null;
