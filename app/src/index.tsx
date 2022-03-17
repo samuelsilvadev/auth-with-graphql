8757e13168aa49ce07250fb8b9c0ebe86badc2e4
Samuel Silva <samuelsilvawb@gmail.com>
2022-03-17 07:38:59 +0000

    chore(app): setup apollo client
    - Added apollo deps
    - Initialized Apollo Provider at the root level

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import App from "./App";
+
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_API_SERVER,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
