e5b9aa2ecac5809a53d4d16449e015890095931d
Samuel Silva <samuelsilvawb@gmail.com>
2022-03-20 15:37:25 +0000

    feat(app): add navigation links on header beased on authenticated user

--- app/src/components/header/Header.tsx
+++ app/src/components/header/Header.tsx
@@ -1,9 +1,55 @@
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
 import { useAuth } from "state/auth/AuthContext";
 
 function Header() {
   const { isAuthenticated } = useAuth();
 
-  return <header>{isAuthenticated ? "Welcome" : "Header"}</header>;
+  return (
+    <Box
+      as="header"
+      height="100px"
+      borderWidth="2px"
+      borderStyle="solid"
+      borderColor="black"
+      p="2"
+    >
+      <Box as="nav" h="100%">
+        <Flex
+          as="ul"
+          alignItems="center"
+          justifyContent="flex-start"
+          gap="4"
+          listStyleType="none"
+          h="100%"
+        >
+          <Box as="li" flexGrow="1">
+            <Link to="/" as={RouterLink}>
+              Home
+            </Link>
+          </Box>
+          {!isAuthenticated ? (
+            <>
+              <li>
+                <Link to="/sign-in" as={RouterLink}>
+                  Sign in
+                </Link>
+              </li>
+              <li>
+                <Link to="/sign-up" as={RouterLink}>
+                  Sign up
+                </Link>
+              </li>
+            </>
+          ) : (
+            <li>
+              <Button>Logout</Button>
+            </li>
+          )}
+        </Flex>
+      </Box>
+    </Box>
+  );
 }
 
 export default Header;
--- app/src/components/layout/Layout.tsx
+++ app/src/components/layout/Layout.tsx
@@ -1,15 +1,24 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import { Box, Container } from "@chakra-ui/react";
 import Header from "components/header/Header";
 
 function Layout() {
   return (
-    <>
+    <Container p="4" maxW="container.lg">
       <Header />
       <Suspense fallback="loading...">
-        <Outlet />
+        <Box
+          marginBlockStart="4"
+          borderWidth="2px"
+          borderStyle="solid"
+          borderColor="black"
+          p="2"
+        >
+          <Outlet />
+        </Box>
       </Suspense>
-    </>
+    </Container>
   );
 }
 
